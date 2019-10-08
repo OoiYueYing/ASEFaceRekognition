1155d89f110019e1007a4f2788726b2c9c991c07
surabhi <surabhi.malani14@gmail.com>
2019-10-08 15:44:40 +0800

    added shink font

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { ThemeProvider } from 'react-native-elements'
 import * as Permissions from 'expo-permissions'
-
+import * as Font from 'expo-font';
 import AppContainer from './navigation'
 
 export default class App extends React.Component {
   componentDidMount() {
     Permissions.askAsync(Permissions.CAMERA_ROLL)
+
+    Font.loadAsync({
+      'shink-regular': require('./assets/fonts/shink/Shink_Regular.ttf'),
+    });
   }
 
   render() {
--- components/Course.js
+++ components/Course.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+
 import {Text, StyleSheet, View, ScrollView,TouchableOpacity, Button, Alert} from 'react-native'
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 const theme = {
@@ -27,15 +28,14 @@ export default class Course extends Component {
       showLab: this.props.showLab,
       courseName: data.courses[this.props.id - 1].name
     });
-
   }
  render() {
-    
+
     const lectureButton = (
       <TouchableOpacity>
-      <Text style={styles.btnText} title="Lecture"> Lecture </Text>
+        <Text style={styles.btnText} title="Lecture"> Lecture </Text>
       </TouchableOpacity>
-    )
+    );
     const tutorialButton = (
         <TouchableOpacity>
         <Text style={styles.btnText} title="Tutorial"> Tutorial </Text>
@@ -47,14 +47,14 @@ export default class Course extends Component {
         </TouchableOpacity>
     );
     if (this.state.showLecture == true && this.state.showTutorial == true && this.state.showLab == true)
-    {   
+    {
         this.state.details = {
         Row: [this.state.courseName],
         tableHead: [lectureButton,tutorialButton ,labButton ],
       }
     }
     else if (this.state.showLecture == true && this.state.showTutorial == true && this.state.showLab == false)
-    {   
+    {
         this.state.details = {
         Row: [this.state.courseName],
         tableHead: [lectureButton,tutorialButton],
@@ -62,20 +62,19 @@ export default class Course extends Component {
     }
     return (
       <View style={styles.container}>
-        <Table borderStyle={{borderWidth: 0},{borderRadius: 50}, {margin:10}}>
+        <Table borderStyle={{borderWidth: 0} ,{borderRadius : 50} ,{margin : 10}}>
           <Row data={this.state.details.Row} style={styles.head} textStyle={styles.textRow}/>
-         
             <Row data = {this.state.details.tableHead} style={styles.head}  />
-            
+
         </Table>
       </View>
-      
+
     )
   }
 }
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 10, paddingTop: 0 , alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center', borderRadius:60},
   head: { height: 40, width: 250, backgroundColor: theme.first },
-  btnText: { fontSize: 20, margin: 6, fontFamily:'notoserif', color: theme.fourth, textAlign: 'center' },
-  textRow: { margin: 6, fontFamily:'Roboto',  color: theme.fourth },
+  btnText: { fontSize: 20, margin: 6, fontFamily:'shink-regular', color: theme.fourth, textAlign: 'center' },
+  textRow: { margin: 6, fontFamily:'shink-regular',  color: theme.fourth },
 });
